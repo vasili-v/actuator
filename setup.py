d142f2f9c985cf2b3adc4c84ef818205f3fcce06
Vasili Vasilyeu <vasili.v@tut.by>
2015-04-24 13:26:59 +0300

    Added actuator package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Actuator",
+    version = "0.1",
+    packages = find_packages(),
+    description = "Actuator library simplifies creation of python scripts",
+    url = "https://github.com/vasili-v/actuator",
+    author = "Vasili Vasilyeu",
+    author_email = "vasili.v@tut.by",
+    license = "MIT",
+    long_description = "Actuator library allows to create python script with " \
+                       "sophisticated argument processing",
+    platforms = ["Darwin", "Linux"],
+)
